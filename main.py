42b2a8b29d84d5da64366e36461e3177fc114153
Resistor-git <partizan-resistor@yandex.ru>
2023-10-22 18:17:01 +0300

    typehining

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+# TODO:
+#  создать список групп и искать в них
+#  прикрутить бота для тг
+#  задеплоить
+
 import os
-import datetime
+from datetime import datetime
+from typing import List, Dict
 
 import requests
 
@@ -11,29 +17,34 @@ ACCESS_TOKEN: str = os.getenv('ACCESS_TOKEN')
 VK_API_VER: str = os.getenv('VK_API_VER')
 NUMBER_OF_POSTS_TO_PARSE: int = int(os.getenv('NUMBER_OF_POSTS_TO_PARSE'))
 
-# group = 'stremyannaya'
-# domain = f'https://api.vk.com/method/wall.get?domain={group}&count=1&filter=owner&&access_token={ACCESS_TOKEN}&v=5.131'
 
-# req = requests.get(domain)
-# print(req.json())
-
-
-def get_posts(group: str) -> list:
+def get_posts(group: str) -> List[Dict]:
+    posts_with_keyword = []
     url = f'https://api.vk.com/method/wall.get?domain={group}&count={NUMBER_OF_POSTS_TO_PARSE}&filter=owner' \
           f'&&access_token={ACCESS_TOKEN}&v={VK_API_VER}'
     request = requests.get(url)
-    # print(request.json()['response']['items'])
     for item in request.json()['response']['items']:
         if 'ComeToSpeak' in item['text']:
-            post_url = f'https://vk.com/wall{item["owner_id"]}_{item["id"]}'
-            post_date = datetime.datetime.fromtimestamp(item['date'])
-            print(f'нашлось в посте от {post_date} по ссылке {post_url}')
-    # print(f'В последних {NUMBER_OF_POSTS_TO_PARSE} объявлениях об английском клубе не нашлось.')
+            posts_with_keyword.append(item)
+    return posts_with_keyword
 
 
-def main():
-    group = 'stremyannaya'
-    get_posts(group)
+def print_results(posts: List) -> None:
+    found = False
+    for post in posts:
+        if 'ComeToSpeak' in post['text']:
+            post_url: str = f'https://vk.com/wall{post["owner_id"]}_{post["id"]}'
+            post_date: datetime = datetime.fromtimestamp(post['date'])
+            print(f'Информация о клубе нашлась в посте от {post_date} по ссылке {post_url}')
+            found = True
+    if not found:
+        print(f'В последних {NUMBER_OF_POSTS_TO_PARSE} постах информации об английском клубе не нашлось.')
+
+
+def main() -> None:
+    group: str = 'stremyannaya'
+    print_results(get_posts(group))
+    print('Парсер закончил свою работу.')
 
 
 if __name__ == '__main__':
