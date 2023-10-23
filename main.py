a61725e94a9bf7e51cc7eac4dd4b5f311f2a62e4
Resistor-git <partizan-resistor@yandex.ru>
2023-10-23 16:01:18 +0300

    Add several groups and keywords in .env to search accordingly

--- main.py
+++ main.py
@@ -5,10 +5,8 @@
 
 import os
 from datetime import datetime
-from typing import List, Dict
 
 import requests
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,34 +14,39 @@ load_dotenv()
 ACCESS_TOKEN: str = os.getenv('ACCESS_TOKEN')
 VK_API_VER: str = os.getenv('VK_API_VER')
 NUMBER_OF_POSTS_TO_PARSE: int = int(os.getenv('NUMBER_OF_POSTS_TO_PARSE'))
+GROUPS: list[str, ...] = os.getenv('GROUPS').split(',')
+KEYWORDS: list[str, ...] = os.getenv('KEYWORDS').split(',')
 
 
-def get_posts(group: str) -> List[Dict]:
+def get_posts(groups: list[str, ...]) -> list[dict]:
+    """
+    Gets posts from the vk groups. Filters them by keywords.
+    """
     posts_with_keyword = []
-    url = f'https://api.vk.com/method/wall.get?domain={group}&count={NUMBER_OF_POSTS_TO_PARSE}&filter=owner' \
-          f'&&access_token={ACCESS_TOKEN}&v={VK_API_VER}'
-    request = requests.get(url)
-    for item in request.json()['response']['items']:
-        if 'ComeToSpeak' in item['text']:
-            posts_with_keyword.append(item)
+    for group in groups:
+        url = f'https://api.vk.com/method/wall.get?domain={group}&count={NUMBER_OF_POSTS_TO_PARSE}&filter=owner' \
+              f'&&access_token={ACCESS_TOKEN}&v={VK_API_VER}'
+        request = requests.get(url)
+        for item in request.json()['response']['items']:
+            for keyword in KEYWORDS:
+                if keyword in item['text']:
+                    posts_with_keyword.append(item)
     return posts_with_keyword
 
 
-def print_results(posts: List) -> None:
+def print_results(posts: list) -> None:
     found = False
     for post in posts:
-        if 'ComeToSpeak' in post['text']:
-            post_url: str = f'https://vk.com/wall{post["owner_id"]}_{post["id"]}'
-            post_date: datetime = datetime.fromtimestamp(post['date'])
-            print(f'Информация о клубе нашлась в посте от {post_date} по ссылке {post_url}')
-            found = True
+        post_url: str = f'https://vk.com/wall{post["owner_id"]}_{post["id"]}'
+        post_date: datetime = datetime.fromtimestamp(post['date'])
+        print(f'Информация о клубе нашлась в посте от {post_date} по ссылке {post_url}')
+        found = True
     if not found:
-        print(f'В последних {NUMBER_OF_POSTS_TO_PARSE} постах информации об английском клубе не нашлось.')
+        print(f'В последних {NUMBER_OF_POSTS_TO_PARSE} постах информации об английских клубах не нашлось.')
 
 
 def main() -> None:
-    group: str = 'stremyannaya'
-    print_results(get_posts(group))
+    print_results(get_posts(GROUPS))
     print('Парсер закончил свою работу.')
 
 
